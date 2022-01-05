d1807a1db39d5eeda13357280a8c847606100d71
Code Master <makovskyi.alexander@gmail.com>
2022-01-05 15:12:48 +0200

    Add default keyboard and support method(sendMessage)

--- src/main/java/org/mo/bots/CommandBot.java
+++ src/main/java/org/mo/bots/CommandBot.java
@@ -1,22 +1,26 @@
 package org.mo.bots;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
 public abstract class CommandBot extends TelegramLongPollingBot {
-
+    //region <Main methods>
     private final Map<String, Method> methods;
 
     public CommandBot() {
         methods = new HashMap<>();
         Class<?> botClass = getClass();
-        for(Method method : botClass.getMethods()) {
-            for(Annotation annotation : method.getDeclaredAnnotations()) {
-                if(annotation instanceof BotCommand) {
+        for (Method method : botClass.getMethods()) {
+            for (Annotation annotation : method.getDeclaredAnnotations()) {
+                if (annotation instanceof BotCommand) {
                     methods.put(((BotCommand) annotation).value(), method);
                 }
             }
@@ -25,12 +29,12 @@ public abstract class CommandBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        if(update.hasMessage() && update.getMessage().hasText()) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
             String input = update.getMessage().getText();
             methods.forEach((string, method) -> {
-                if(input.equals(string)) {
+                if (input.equals(string)) {
                     try {
-                        method.invoke(this,update.getMessage());
+                        method.invoke(this, update.getMessage());
                     } catch (Exception exception) {
                         exception.printStackTrace();
                     }
@@ -38,5 +42,19 @@ public abstract class CommandBot extends TelegramLongPollingBot {
             });
         }
     }
-
+    //endregion
+    //region<Support methods>
+    protected void sendMessage(String chatId, String text, ReplyKeyboard keyboard) {
+        SendMessage sendMessage = SendMessage.builder()
+                .chatId(chatId)
+                .text(text)
+                .replyMarkup(keyboard)
+                .build();
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException exception) {
+            exception.printStackTrace();
+        }
+    }
+    //endregion
 }
--- src/main/java/org/mo/bots/PizzaBot.java
+++ src/main/java/org/mo/bots/PizzaBot.java
@@ -1,6 +1,32 @@
 package org.mo.bots;
 
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+
 public class PizzaBot extends CommandBot{
+    //region<Command methods>
+    @BotCommand("/start")
+    public void start(Message message) {
+        ReplyKeyboardMarkup keyboard = ReplyKeyboardMarkup.builder()
+                .resizeKeyboard(true)
+                .keyboardRow(createKeyboardRow("\uD83D\uDC3CPANDA PIZZA\uD83D\uDC3C"))
+                .keyboardRow(createKeyboardRow("\uD83C\uDF55Меню", "\uD83D\uDC3EАкції"))
+                .keyboardRow(createKeyboardRow("\uD83D\uDCE6Кошик"))
+                .build();
+        sendMessage(message.getChatId().toString(), "Повідомлення 1", keyboard);
+    }
+    //endregion
+    //region<Support methods>
+    private KeyboardRow createKeyboardRow(String... buttons) {
+        KeyboardRow row = new KeyboardRow();
+        for(String button : buttons) {
+            row.add(button);
+        }
+        return row;
+    }
+    //endregion
+    //region<Settings methods>
     @Override
     public String getBotUsername() {
         return System.getenv("BotUsername");
@@ -10,4 +36,5 @@ public class PizzaBot extends CommandBot{
     public String getBotToken() {
         return System.getenv("BotToken");
     }
+    //endregion
 }
